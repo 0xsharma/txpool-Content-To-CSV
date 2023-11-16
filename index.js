413bf699230a0a78914e2a131b0df2ddaa5b28ea
Shivam Sharma <shivam691999@gmail.com>
2023-11-16 15:48:50 +0530

    fix : minor fix

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ var HTTPSWEB3 = 'http://localhost:8545'
 
 const timer = ms => new Promise(res => setTimeout(res, ms))
 
-async function getTxPoolContent(){
-    
-    var res 
-    await axios.post(HTTPSWEB3 ,{
+async function getTxPoolContent() {
+    var res
+    await axios.post(HTTPSWEB3, {
         jsonrpc: '2.0',
         method: 'txpool_content',
         params: [],
         id: 1
     }, {
         headers: {
-        'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
         },
     }).then((response) => {
         res = response
@@ -23,32 +22,40 @@ async function getTxPoolContent(){
     return res.data.result
 }
 
-async function main(){
+async function main() {
     var lastStartingTime = Math.floor(new Date().getTime() / 1000)
-    txpoolContent = await getTxPoolContent()
+    var txpoolContent = await getTxPoolContent()
     pending = txpoolContent.pending
     fs.appendFile(`./output/out-${lastStartingTime}.csv`, 'GasPrice' + '\n', function (err) {
         if (err) throw err;
         console.log('Added');
     });
     for (var [key, value] of Object.entries(pending)) {
-            
+
         // eslint-disable-next-line no-unused-vars
         for (var [nonce, txobject] of Object.entries(value)) {
-            txobject.gasPrice = parseInt(txobject.gasPrice,16)
-            fs.appendFile(`./output/out-${lastStartingTime}.json`, JSON.stringify(txobject) + '\n', function (err) {
-                    if (err) throw err;
-                    console.log('Added');
-            });
-
-            fs.appendFile(`./output/out-${lastStartingTime}.csv`, JSON.stringify(txobject.gasPrice) + ',' + JSON.stringify(txobject.hash) + '\n', function (err) {
-                if (err) throw err;
-                console.log('Added');
-            });
-
-            break
-            
+            if (txobject.gasPrice !== undefined) {
+                txobject.gasPrice = parseInt(txobject.gasPrice, 16)
+            }
+            if (txobject.maxFeePerGas !== undefined) {
+                txobject.maxFeePerGas = parseInt(txobject.maxFeePerGas, 16)
+            }
+            if (txobject.maxPriorityFeePerGas !== undefined) {
+                txobject.maxPriorityFeePerGas = parseInt(txobject.maxPriorityFeePerGas, 16)
+            }
         }
+        fs.appendFile(`./output/out-${lastStartingTime}b.json`, JSON.stringify(txobject) + '\n', function (err) {
+            if (err) throw err;
+            console.log('Added');
+        });
+
+        fs.appendFile(`./output/out-${lastStartingTime}.csv`, JSON.stringify(txobject.gasPrice) + ',' + JSON.stringify(txobject.maxFeePerGas) + ',' + JSON.stringify(txobject.maxPriorityFeePerGas) + ',' + JSON.stringify(txobject.hash) + '\n', function (err) {
+            if (err) throw err;
+            console.log('Added');
+        });
+
+        break
+
     }
 
     await timer(2000)
